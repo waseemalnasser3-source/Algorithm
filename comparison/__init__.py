d738fd0562e2bcf83b7ed6115ec84d6518c4fc51
rania837 <raniaabdaljalil26@gmail.com>
2025-11-02 22:38:40 +0300

    Add files via upload

---
+++ comparison/__init__.py
@@ -0,0 +1,11 @@
+"""
+Comparative Analysis & Benchmarking Module
+Statistical evaluation and performance comparison framework
+Developed by: Hana Saleh (Analytics Specialist)
+"""
+
+from .traditional_methods import TraditionalFeatureSelection
+from .statistical_analysis import StatisticalAnalysis
+
+__all__ = ['TraditionalFeatureSelection', 'StatisticalAnalysis']
+
